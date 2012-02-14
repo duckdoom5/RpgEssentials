1b0215a5036e4fd72c212af404a8f111aafb268a
Robert Xu <robxu9@gmail.com>
2012-02-13 23:13:39 -0500

    Possible fix to MyConfiguration failure.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- me/duckdoom5/RpgEssentials/config/Configuration.java
+++ me/duckdoom5/RpgEssentials/config/Configuration.java
@@ -9,15 +9,15 @@ public class Configuration {
 	
 	private static final String h = "[RpgEssentials] ";
 
-	public static final MyConfiguration config;
-	public static final MyConfiguration block;
-	public static final MyConfiguration entity;
-	public static final MyConfiguration generator;
-	public static final MyConfiguration items;
-	public static final MyConfiguration level;
-	public static final MyConfiguration players;
-	public static final MyConfiguration region;
-	public static final MyConfiguration store;
+	public static MyConfiguration config;
+	public static MyConfiguration block;
+	public static MyConfiguration entity;
+	public static MyConfiguration generator;
+	public static MyConfiguration items;
+	public static MyConfiguration level;
+	public static MyConfiguration players;
+	public static MyConfiguration region;
+	public static MyConfiguration store;
 	
 	static {
 		config = new MyConfiguration();
@@ -31,46 +31,55 @@ public class Configuration {
 		store = new MyConfiguration();
 		
 		if (load(config,"config.yml")){
+			config = MyConfiguration.loadConfiguration("plugins/RpgEssentials/config.yml");
 			Config.set();
 			save(config);
 		}
 		
 		if (load(block,"blocks.yml")){
+			block = MyConfiguration.loadConfiguration("plugins/RpgEssentials/blocks.yml");
 			BlockConfig.set();
 			save(block);
 		}
 		
 		if (load(entity,"Entities.yml")){
+			entity = MyConfiguration.loadConfiguration("plugins/RpgEssentials/Entities.yml");
 			EntityConfig.set();
 			save(entity);
 		}
 		
 		if (load(generator,"worldgenerator.yml")){
+			generator = MyConfiguration.loadConfiguration("plugins/RpgEssentials/worldgenerator.yml");
 			GeneratorConfig.set();
 			save(generator);
 		}
 		
 		if (load(items,"items.yml")){
+			items = MyConfiguration.loadConfiguration("plugins/RpgEssentials/items.yml");
 			ItemConfig.set();
 			save(items);
 		}
 		
 		if (load(level,"leveling.yml")){
+			level = MyConfiguration.loadConfiguration("plugins/RpgEssentials/Leveling.yml");
 			LevelConfig.set();
 			save(level);
 		}
 		
 		if (load(players, "Temp/Players.yml")){
+			players = MyConfiguration.loadConfiguration("plugins/RpgEssentials/Temp/Players.yml");
 			PlayerConfig.set();
 			save(players);
 		}
 		
 		if (load(region,"regions.yml")){
+			region = MyConfiguration.loadConfiguration("plugins/RpgEssentials/regions.yml");
 			RegionConfig.set();
 			save(region);
 		}
 		
 		if (load(store,"store.yml")){
+			store = MyConfiguration.loadConfiguration("plugins/RpgEssentials/store.yml");
 			StoreConfig.set();
 			save(store);
 		}
