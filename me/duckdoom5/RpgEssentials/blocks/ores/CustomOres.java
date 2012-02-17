b3a0e1ba2626a6a249d80a3c5d549617b24c6c5f
Jelle <j_e_l_l_e@hotmail.com>
2012-02-17 13:26:51 +0100

    updated

--- me/duckdoom5/RpgEssentials/blocks/ores/CustomOres.java
+++ me/duckdoom5/RpgEssentials/blocks/ores/CustomOres.java
@@ -6,22 +6,28 @@ import org.getspout.spoutapi.material.CustomItem;
 import me.duckdoom5.RpgEssentials.RpgEssentials;
 
 public class CustomOres extends CustomOresDesign {
+	private float hard = null;
+	private float friction = null;
+	private int light = 1;
+	private int amount = 1;
+	private CustomBlock = null;
+	private CustomItem = null;
 
 	public CustomOres(RpgEssentials plugin,String name, int textureid, int freq, int minY, int maxY)
 	{
-		super(plugin, name, textureid, freq, minY, maxY);
+		super(plugin, name, textureid, freq, minY, maxY, hard, light, friction, drop, amount);
 	}
 	public CustomOres(RpgEssentials plugin,String name, int textureid, int freq, int minY, int maxY, float hard, int light, float friction)
 	{
-		super(plugin, name, textureid, freq, minY, maxY, hard, light, friction);
+		super(plugin, name, textureid, freq, minY, maxY, hard, light, friction, drop, amount);
 	}
 	public CustomOres(RpgEssentials plugin,String name, int textureid, int freq, int minY, int maxY, CustomItem drop)
 	{
-		super(plugin, name, textureid, freq, minY, maxY, drop ,amount);
+		super(plugin, name, textureid, freq, minY, maxY, hard, light, friction, drop, amount);
 	}
 	public CustomOres(RpgEssentials plugin,String name, int textureid, int freq, int minY, int maxY, CustomItem drop, int amount)
 	{
-		super(plugin, name, textureid, freq, minY, maxY, drop ,amount);
+		super(plugin, name, textureid, freq, minY, maxY, hard, light, friction, drop, amount);
 	}
 	public CustomOres(RpgEssentials plugin,String name, int textureid, int freq, int minY, int maxY, CustomBlock drop, float hard, int light, float friction)
 	{
