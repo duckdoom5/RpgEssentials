6f725863ec37933b0ed3e82e26c63605b887ebec
Jelle <j_e_l_l_e@hotmail.com>
2012-03-15 20:43:56 +0100

    removing old files

--- me/duckdoom5/RpgEssentials/config/BankConfig.java
+++
@@ -1,8 +0,0 @@
-package me.duckdoom5.RpgEssentials.config;
-
-public class BankConfig {
-
-	public static void set(){
-		
-	}
-}
--- me/duckdoom5/RpgEssentials/config/MailConfig.java
+++
@@ -1,7 +0,0 @@
-package me.duckdoom5.RpgEssentials.config;
-
-public class MailConfig {
-	public static void set(){
-		// nothing done here...
-	}
-}
